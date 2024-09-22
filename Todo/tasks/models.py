1f6e360411d552de4c1a21378b785b80b7dc99d1
Habib <habubyies@gmail.com>
2024-09-22 13:34:54 +0300

    Tasks model created

--- Todo/tasks/models.py
+++ Todo/tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class task(models.Model):
+    Title = models.CharField(max_length=255)
+    Done = models.BooleanField(default=False)
+    Date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        if (self.Done):
+            return f'{self.Title}: Done'
+        else:
+            return f'{self.Title}: Active'
